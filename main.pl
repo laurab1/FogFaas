42066eeb7f0adc1f9edb39ef1a48825f1c862ffc
laurab1 <l.bussi1@studenti.unipi.it>
2020-05-25 17:20:23 +0200

    simplified example

--- main.pl
+++ main.pl
@@ -2,7 +2,9 @@
 :- consult('infra.pl').
 :- consult('fogfaas.pl').
 
-%query(placeFunctions(ann, (service1, n1), [(service1, n1),(service2,n4)], send([x], service2), [], R, [], C)).
+query(placeFunctions(default, (service1, n1), [(service1, n1),(service2,n2)], seq(mult,sum), [], R, [], C)).
+
+query(placeServices(default, [service1, service2], [], P, [], C)).
 
 query(placeApp(default, app1, SP, FP)).
 
@@ -14,4 +16,4 @@ query(placeApp(default, app1, SP, FP)).
 
 %query(ctx(ann, seq(sum, send([x], service1, 1)), L, [])).
 
-%query(ctx(ann, ife(sum, sum, send([x], service1, 1)), L, [x], Env)).
+%query(ctx(default, ife(sum, sum, send([x], service1, 1)), L, [x], Env)).
