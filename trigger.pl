fd24cc0bac321d541d25f600b79cf52458e54710
azecoder <tarbiz.haji@gmail.com>
2020-05-25 19:09:50 +0200

    fire func for trigger

--- trigger.pl
+++ trigger.pl
@@ -1,12 +1,21 @@
 
+ts(z).
+s(x).
+l(y).
+l(t).
 
-trigger(triggerX, sum, rule1).
-trigger(triggerY, div, rule2).
-trigger(triggerY, sum, rule3).
+
+
+%trigger(TId, Params, Prog, Rule).
+trigger(triggerX, [x,y], sum, rule1).
+trigger(triggerY, [z,z], div, rule2).
+trigger(triggerZ, [x,y], sum, rule3).
+
+
 
 placeTriggers(AOp, [], T, T).
 placeTriggers(AOp, [TId|Rest], Placement, [(TId, NId)|NewPlacement], Caps, NewCaps) :-
-    trigger(TId, Prog, Rule),
+    trigger(TId, Params, Prog, Rule),
     node(NId, OpN, HwCaps, SPlats, _, CostPU, NodeLoc),
     member(NodeLoc, Geo),
     subset(PReqs, SPlats),
@@ -15,9 +24,25 @@ placeTriggers(AOp, [TId|Rest], Placement, [(TId, NId)|NewPlacement], Caps, NewCa
     placeTriggers(AOp, Rest, Placement, NewPlacement, TmpCaps, NewCaps).
 
 
-
-
 % INFORMATION FLOW
 labelTrigger(AOp, TId, L) :- 
     trigger(TId, P, _), 
     ctx(AOp, P, L).
+
+
+
+
+placeFunctions(AOp, SId, fire(TId), Placement, [(SId, FId, NId)|Placement], Caps, NewCaps) :-
+    trigger(TId, Params, FId, Rule),
+    node(NId, OpN, HwCaps, SPlats, FPlats, CostPU, Geo),
+    trusts2(AOp, OpN),
+    checkPlatforms(PReqs, FPlats),
+    checkContext(AOp, Args, NId, OpN, Geo, L),
+    HwReqs =< HwCaps, checkHw(HwCaps, HwReqs, NId, Caps, NewCaps).  
+
+
+
+ctxFire(AOp, fire(TId), L) :-
+    trigger(TId, Args, FId, _),
+    func(FId, Args, _, _, _),
+    labelF(AOp, Args, L). 
