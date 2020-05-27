899db53faea54e1083b4c6669275d87a60d32bb3
laurab1 <l.bussi1@studenti.unipi.it>
2020-05-27 12:34:52 +0200

    minor fix

--- infra.pl
+++ infra.pl
@@ -1,24 +1,29 @@
-node(n1, amazon, 2, [ubuntu], [python, rust], 0.003, eu).
+node(n1, amazon, 5, [ubuntu], [python, rust], 0.003, eu).
 encrypted_storage(n1).
 firewall(n1).
 
-node(n2, amazon, 2, [ubuntu, sql], [python, rust, kotlin], 0.001, us).
+node(n2, amazon, 6, [ubuntu, sql], [python, rust, kotlin], 0.001, us).
 encrypted_storage(n2).
 firewall(n2).
 
 node(n3, ibm, 3, [sql], [kotlin, java, javascript], 0.003, eu).
 firewall(n3).
 
-node(n4, amazon, 2, [ubuntu, sql], [python, kotlin], 0.001, us).
+node(n4, amazon, 4, [ubuntu, sql], [python, kotlin], 0.001, us).
+encrypted_storage(n4).
 firewall(n4).
 
-node(n5, amazon, 2, [ubuntu, sql], [python, rust, java, javascript], 0.001, eu).
+node(n5, amazon, 7, [ubuntu, sql], [python, rust, java, javascript], 0.001, eu).
 encrypted_storage(n5).
 firewall(n5).
 
-node(n6, azure, 3, [ubuntu, sql], [python], 0.004, eu).
+node(n6, azure, 4, [ubuntu, sql], [python, kotlin], 0.004, eu).
 encrypted_storage(n6).
 firewall(n6).
+
+node(n7, azure, 6, [ubuntu, sql], [python, kotlin], 0.004, eu).
+encrypted_storage(n7).
+firewall(n7).
 
 %link(l1, 1, [n1, n2]).
 0.7::link(l1, 1, [n1, n2]).
